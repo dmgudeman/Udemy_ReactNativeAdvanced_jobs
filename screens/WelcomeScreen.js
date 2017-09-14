40991b1d67aa670ae7fd1b2589546b243f861e50
davidmgudeman <davidmgudeman@gmail.com>
2017-09-14 14:18:58 -0700

    The slide component has been roughed out and wired up

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import Slides from '../components/Slides';
 
+const SLIDE_DATA = [
+  { text: 'Welcom to JobApp'},
+  { text: 'Job Finder will help you get a local job'}
+
+]
+
 class WelcomeScreen extends Component{
   render() {
     return (
-      <View>
-        <Slides />
-      </View>
+        <Slides data={SLIDE_DATA} />
     );
   }
 }
