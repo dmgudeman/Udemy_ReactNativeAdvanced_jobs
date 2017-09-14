b6b5ac7232db1e9e837627a66969b3bd72003080
davidmgudeman <davidmgudeman@gmail.com>
2017-09-14 11:28:30 -0700

    added a settings button to the header in the ReviewScreen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
-class ReviewScreen extends Component{
+class ReviewScreen extends Component {
+  // The router reads this first
+  static navigationOptions = {
+    title: 'Review Jobs',
+    headerRight: <Button 
+      title="Settings"
+      onPress={ () => console.log('Hi there') }
+    />
+  }
+  
+
   render() {
     return (
       <View>
