16266635535625fc01db1908c27ea5a748ed0473
davidmgudeman <davidmgudeman@gmail.com>
2017-09-14 15:26:07 -0700

    added the button to the last welcomeScreen slide

--- components/Slides.js
+++ components/Slides.js
@@ -1,24 +1,43 @@
 
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
-renderSlides() {
-  return this.props.data.map((slide) => {
-    return (
-      <View key={slide.text} style={styles.slideStyle} >
-        <Text style={styles.slideText}>{slide.text}</Text>
-      </View>
-    )
-    
-  });
-}
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1) {
+      return (
+      <Button 
+         title="Onwards!"
+         raised
+         buttonStyle={styles.buttonStyle}
+      />
+      );
+    }
+  }
+  renderSlides() {
+    return this.props.data.map((slide, index) => {
+      return (
+        <View 
+          key={slide.text} 
+          style={[styles.slideStyle, {backgroundColor: slide.color}]} 
+        >
+          <Text style={styles.textStyle}>{slide.text}</Text>
+          {this.renderLastSlide(index)}
+        </View>
+      )
+      
+    });
+  }
 
   render() {
     return (
       <ScrollView
         horizontal
         style={{ flex: 1 }}
+        pagingEnabled
       >
         {this.renderSlides()}
       </ScrollView>
@@ -30,10 +49,17 @@ const styles = {
   slideStyle: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    width: SCREEN_WIDTH
   },
-  slideText: {
-    fontSize: 30
+  textStyle: {
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center'
+  },
+  buttonStyle: {
+    backgroundColor:'#0288D1',
+    marginTop: 15
   }
 }
 
