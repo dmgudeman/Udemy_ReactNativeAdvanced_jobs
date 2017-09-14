939295a92e4b9dc2142466d95765ab481df73959
davidmgudeman <davidmgudeman@gmail.com>
2017-09-14 10:04:43 -0700

    scaffolded out first two tab bars

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+
+export default class App extends React.Component {
+  render() {
+    const MainNavigator = TabNavigator ({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen }
+        })
+      }
+    });
+
+    return (
+      <View style={styles.container}>
+        <MainNavigator />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+});
+
+Expo.registerRootComponent(App);
