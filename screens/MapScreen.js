4755a5d86fe1ecc0a86765b231cf495df0ba0dc6
davidmgudeman <davidmgudeman@gmail.com>
2017-09-15 14:48:18 -0700

    map is using a region and is updating state as it is moved

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
 
 class MapScreen extends Component{
   state = {
+    mapLoaded: false,
     region: {
       longitude: -122,
       latitude: 37,
@@ -11,13 +12,31 @@ class MapScreen extends Component{
       latitudeDelta: 0.09
     }
   }
+  
+  componentDidMount() {
+    this.setState({mapLoaded: true });
+  }
+
+  onRegionChangeComplete = (region) => {
+    this.setState({ region })
+  }
 
   render() {
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+
     return (
       <View style={{ flex:1 }}>
         <MapView 
           region ={this.state.region}
-          style={{ flex:1 }} />
+          style={{ flex:1 }} 
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
       </View>
     );
   }
