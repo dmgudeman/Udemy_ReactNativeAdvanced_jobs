c31e61af3ec4833cd4192cc51554d725954fd781
davidmgudeman <davidmgudeman@gmail.com>
2017-09-15 11:53:45 -0700

    routed to map screen after login

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class AuthScreen extends Component{
   componentDidMount() {
     this.props.facebookLogin();
+    this.onAuthComplete(this.props);
+
+    // uncomment to test login process
+    // AsyncStorage.removeItem('fb_token');
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.onAuthComplete(nextProps);
+  }
+
+  onAuthComplete(props) {
+    if (props.token) {
+      this.props.navigation.navigate('map');
+    }
   }
 
 
@@ -22,4 +36,7 @@ class AuthScreen extends Component{
   }
 }
 
-export default connect(null, actions)(AuthScreen);
+function mapStateToProps({ auth }) {
+  return { token: auth.token }
+}
+export default connect(mapStateToProps, actions)(AuthScreen);
