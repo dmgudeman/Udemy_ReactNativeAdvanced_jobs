83fd94a5a775cf65c6b8a409be8c343a3f123e92
davidmgudeman <davidmgudeman@gmail.com>
2017-09-16 10:25:24 -0700

    pulled the list of jobs into DeckScreen using connect and mapStateToProps methods

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 
 class DeckScreen extends Component{
   render() {
     return (
       <View>
-        <Text> DeckScreen </Text>
-        <Text> DeckScreen </Text>
-        <Text> DeckScreen </Text>
-        <Text> DeckScreen </Text>
-        <Text> DeckScreen </Text>
+        
       </View>
     );
   }
 }
 
-export default DeckScreen;
+
+// using jobs to signify the jobs object from the Indeed API and 
+// jobs is now used as the key for only the results property of the jobs object
+// returned.
+function mapStateToProps({ jobs }) {
+  return { jobs: jobs.result };
+}
+
+export default connect(mapStateToProps)(DeckScreen);
