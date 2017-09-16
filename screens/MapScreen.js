31483bc33de65cf2bb0c13becfacdbcd74aa2c8d
davidmgudeman <davidmgudeman@gmail.com>
2017-09-16 09:57:41 -0700

    the MapView button was hooked up and tested with console.log

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import { Button } from 'react-native-elements';
 
 class MapScreen extends Component{
   state = {
@@ -20,6 +23,10 @@ class MapScreen extends Component{
   onRegionChangeComplete = (region) => {
     this.setState({ region })
   }
+  
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region);
+  }
 
   render() {
     if (!this.state.mapLoaded) {
@@ -37,9 +44,25 @@ class MapScreen extends Component{
           style={{ flex:1 }} 
           onRegionChangeComplete={this.onRegionChangeComplete}
         />
+        <View style={styles.buttonContainer}>
+          <Button 
+            large
+            title="Search This Area"
+            backGroundColor="#009688"
+            icon={{ name: 'search'}}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
-
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+}
+export default connect(null, actions)(MapScreen);
