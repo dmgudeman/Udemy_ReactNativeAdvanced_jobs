affb06a12a0375754d2bcd79bc2ed3fa2d7d6be0
davidmgudeman <davidmgudeman@gmail.com>
2017-09-19 15:28:53 -0700

    added the logic for checking for a push notification token and creating one if necessary

---
+++ services/push_notifications.js
@@ -0,0 +1,19 @@
+
+import { Permissions, Notifications } from 'expo';
+
+
+export default async () => {
+  let previousToken = await AsyncStorage.getItem('pushtoken');
+
+  if (previousToken) {
+    return;
+  } else {
+    let { status } = await Permissions.askAsync(Permissions.REMOTE_NOTIFICATIONS);
+    
+    if (status !== 'granted'){
+      return;
+    }
+
+    let token = await Notifications.getExponentPushTokenAsync();
+  }
+};
